3443d3e0c42d50d7562ad956af74ceb45ebc5578
Cherish <shmilycherish@gmail.com>
2016-09-01 17:53:35 +0800

    [ng2-3]add app-bar

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -1,7 +1,9 @@
 import {Component} from "@angular/core";
+import {AppBar} from "./app-bar";
 
 @Component({
     selector: 'home',
-    template: require('./home.html')
+    template: require('./home.html'),
+    directives: [AppBar]
 })
 export class Home{}
