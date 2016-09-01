475477bbfd14f7d6b8275a9a106c0ecce7376227
Cherish <shmilycherish@gmail.com>
2016-09-01 19:36:22 +0800

    [ng2-5]create note-card.

--- src/app/home/notes/index.ts
+++ src/app/home/notes/index.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
-
+import {NoteCard} from "./note-card";
 
 @Component({
     selector: 'notes',
     template: require('./notes.html'),
-    styles: [require('./notes.css')]
+    styles: [require('./notes.css')],
+    directives: [NoteCard]
 })
 export class Notes{}
