a3e5f494afbd43d04f39e8c4976dd7a93700c186
Cherish <shmilycherish@gmail.com>
2016-09-08 17:24:42 +0800

    [ng2-7]display create note panel

--- src/app/home/notes/index.ts
+++ src/app/home/notes/index.ts
@@ -1,11 +1,12 @@
 import {Component} from "@angular/core";
 import {NoteCard} from "./note-card";
+import {NoteCreator} from "./note-creator";
 
 @Component({
     selector: 'notes',
     template: require('./notes.html'),
     styles: [require('./notes.css')],
-    directives: [NoteCard]
+    directives: [NoteCard, NoteCreator]
 })
 export class Notes{
     notes = [{
---
+++ src/app/home/notes/note-creator/index.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'note-creator',
+    template: require('./note-creator.html'),
+    styles: [require('./note-creator.css')]
+})
+export class NoteCreator{
+
+}
